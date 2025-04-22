2b0a786094b2572ac223384e8a2dba716d4e7fc7
Haseen202 <faisalabdulaziz493@gmail.com>
2025-04-22 15:22:19 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,11 @@
+public class name
+{
+public static void main(String[] args)
+{
+int num=10;
+if(num>0)
+{
+System.out.println("the number is positive.");
+}
+}
+}
---
+++ number.java
@@ -0,0 +1,9 @@
+public class number
+{
+public static void main(String[] args)
+{
+int a=13,b=2;
+int sum=a+b;
+System.out.println(sum);
+}
+}
